11874c5fe4b09840b4450270c7036e2b19d20bd7
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-05 15:53:05 -0500

    Primer script y movimiento del jugador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+
+        transform.position = new Vector3(mousePos.x,mousePos.y, transform.position.z)
     }
 }
