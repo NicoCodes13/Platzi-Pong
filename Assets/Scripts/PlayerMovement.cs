64dfb5e9bd9b7242c0cfa40a8b5aac1f5054da05
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-05 22:57:43 -0500

    Restringiendo la salida de la paleta

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-
+    SpriteRenderer playerSpriteRender;
+    void Start () {
+        playerSpriteRender = GetComponent<SpriteRenderer> (); //*trae la info del render 
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(Input.mousePosition);
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
-        Vector2 mousePos2d = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+    void Update () {
+        Vector2 mousePos2d = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
-        transform.position = mousePos2d;
+        transform.position = new Vector2 (transform.position.x, Mathf.Clamp (mousePos2d.y,-3.9f,3.9f));
     }
 }
