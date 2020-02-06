4f6953ffcd6ef34d2e7e87ae7670c2c495f27391
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-05 23:40:04 -0500

    Posicion inicial de la pelota

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+    public Transform paddle;
+    bool gameStarted = false;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+    // Update is called once per frame
+    void Update () {
+        if (!gameStarted) {
+
+            float posDif = paddle.position.x - transform.position.x;
+            transform.position = new Vector2 (paddle.position.x - posDif, paddle.position.y);
+            if(Input.GetMouseButtonDown(0))
+            {
+                gameStarted = true;
+            }
+        }
+    }
+}
